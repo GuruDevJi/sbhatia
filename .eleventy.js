5166a254525fa7c1b2431436fdaabbf38110f680
saurabh@sbhatia.com <saurabh@sbhatia.com>
2026-01-07 03:48:13 -0500

    fix var issue

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
-
+  eleventyConfig.addGlobalData("var", 
+    JSON.parse(fs.readFileSync('./src/_data/var.json'))
+  );  
   return {
     dir: {
       input: "src",
